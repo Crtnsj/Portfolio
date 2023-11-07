479848a0e4bc433734b590e46346f88207091c77
Corentin SAN JUAN <crtnsj.pro@gmail.com>
2023-11-07 12:20:45 +0100

    second published

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-base: "/Portfolio";
+base: "https://crtnsj.github.io/Portfolio/";
 
 export default defineConfig({
   plugins: [react()],
