45fb4ea40d63ebbe37724a4ae70378a6e6e219d3
Corentin SAN JUAN <crtnsj.pro@gmail.com>
2023-11-07 14:36:16 +0100

    Deploy App 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-base: "/Portfolio/";
 
 export default defineConfig({
+  base: "/Portfolio/",
   plugins: [react()],
 });
