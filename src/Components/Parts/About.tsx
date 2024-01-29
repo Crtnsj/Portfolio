09a2eab180978ff053db182f135480ec60e6dcc8
crtnsj <crtnsj.pro@gmail.com>
2024-01-29 10:32:53 +0100

    Ajout de contenu

--- src/Components/Parts/About.tsx
+++ src/Components/Parts/About.tsx
@@ -1,5 +1,6 @@
 import Timeline from "../Timeline";
 import CV from "../../assets/CV_Corentin_Sanjuan.pdf";
+import tableauCompentence from "../../assets/CV_Corentin_Sanjuan.pdf";
 
 
 const About = () => {
@@ -18,10 +19,8 @@ const About = () => {
             Organisations) à l'École ISITECH de Lyon, option SLAM (Solutions
             Logicielles et Applications Métier).
             <br />
-            <br /> Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Enim sed iusto expedita consequatur explicabo iure. Earum provident
-            voluptatum eaque sint dolorum architecto, saepe molestiae, quis
-            magnam dolores, quidem ratione! Tenetur.
+            <br /> Actuellement toujours en études je vous presente par le biais de mon portfolio mes experience professionnelles 
+            et mes différents projets réalisés au cours de mes deux années de BTS SIO option SLAM
           </p>
           <div>
           <button className="cvButton">
@@ -30,14 +29,40 @@ const About = () => {
             </a>
 
           </button>
-          <button className="cvButton">
-            <a href={CV} target="_blank">
+          <button className="cvButton tableauCompetence">
+            <a href={tableauCompentence} target="_blank">
               Ma fiche de compétence
             </a>
           </button>
           </div>
         </div>
         <div className="about__pic"></div>
+        
+      </div>
+      <div></div>
+      <div className="experiencePro">
+        <h2>Mes expériences professionnelles</h2>
+        <div>
+        <h3>Administrateur systemes et réseaux</h3>
+        <p>Grand Bourg Habitat - 01000 Bourg-en-Bresse | Août 2022 - Aujourd’hui</p>
+        <li>Support aux utilisateurs</li>
+        <li>Migration vers un nouveaux plan d’adressage IP</li>
+        <li>Création et personnalisation d’un GLPI</li>
+        <li>Participation aux projets du Service Informatique </li>
+        </div>
+        <div>
+        <h3>Technicien systèmes et réseaux</h3>
+        <p>ACG-Synergies - 01000 Bourg-en-Bresse | Janvier 2022 - Mars 2022 | Stage</p>
+        <li>Maintien en Condition Opérationnel (MCO) de l’infrastructure</li>
+        <li>Recherche et mise en place d’outils et de procédures pour améliorer le MCO</li>
+        </div>
+        <div>
+        <h3>Technicien systèmes et réseaux</h3>
+        <p>Préfecture de l'AIN - 01000 Bourg-en-Bresse | Mai 2021 - Juillet 2021 | Stage</p>
+        <li>Déploiement de postes informatiques</li>
+        <li>Gestion de tickets</li>
+        <li>Gestion d’inventaire.</li>
+        </div>
       </div>
     </div>
   );
