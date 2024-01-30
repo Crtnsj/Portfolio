3566c9be1d44df8c479adf2318bc6c397dc753e3
crtnsj <crtnsj.pro@gmail.com>
2024-01-30 16:11:14 +0100

    petit correctif -> projets

--- src/Components/Parts/About.tsx
+++ src/Components/Parts/About.tsx
@@ -1,7 +1,6 @@
 import Timeline from "../Timeline";
 import CV from "../../assets/CV_Corentin_Sanjuan.pdf";
 import tableauCompentence from "../../assets/CV_Corentin_Sanjuan.pdf";
-import logo_GBH from "../../assets/logo_GBH.png";
 
 const About = () => {
   return (
@@ -15,13 +14,10 @@ const About = () => {
             <br />
             <br />
             Je suis étudiant en deuxieme année de
-            <strong> BTS SIO</strong>, (Services Informatiques aux
-            Organisations) à l'École ISITECH de Lyon, option SLAM (Solutions
-            Logicielles et Applications Métier).
+            <strong> BTS SIO</strong>, (Services Informatiques aux Organisations) à l'École ISITECH de Lyon, option SLAM (Solutions Logicielles et Applications Métier).
             <br />
-            <br /> Actuellement toujours en études je vous presente par le biais
-            de mon portfolio mes experience professionnelles et mes différents
-            projets réalisés au cours de mes deux années de BTS SIO option SLAM
+            <br /> Actuellement toujours en études je vous presente par le biais de mon portfolio mes experience professionnelles et mes différents projets réalisés au cours de mes deux années de BTS
+            SIO option SLAM
           </p>
           <div>
             <button className="cvButton">
@@ -45,10 +41,7 @@ const About = () => {
           <div className="logo_exp logo_gbh"></div>
           <div className="content_exp">
             <h3>Administrateur systemes et réseaux</h3>
-            <p>
-              Grand Bourg Habitat - 01000 Bourg-en-Bresse | Août 2022 -
-              Aujourd’hui
-            </p>
+            <p>Grand Bourg Habitat - 01000 Bourg-en-Bresse | Août 2022 - Aujourd’hui</p>
             <li>Support aux utilisateurs</li>
             <li>Migration vers un nouveaux plan d’adressage IP</li>
             <li>Création et personnalisation d’un GLPI</li>
@@ -59,27 +52,16 @@ const About = () => {
           <div className="logo_exp logo_acg"></div>
           <div className="content_exp">
             <h3>Technicien systèmes et réseaux</h3>
-            <p>
-              ACG-Synergies - 01000 Bourg-en-Bresse | Janvier 2022 - Mars 2022 |
-              Stage
-            </p>
-            <li>
-              Maintien en Condition Opérationnel (MCO) de l’infrastructure
-            </li>
-            <li>
-              Recherche et mise en place d’outils et de procédures pour
-              améliorer le MCO
-            </li>
+            <p>ACG-Synergies - 01000 Bourg-en-Bresse | Janvier 2022 - Mars 2022 | Stage</p>
+            <li>Maintien en Condition Opérationnel (MCO) de l’infrastructure</li>
+            <li>Recherche et mise en place d’outils et de procédures pour améliorer le MCO</li>
           </div>
         </div>
         <div>
           <div className="logo_exp logo_pref"></div>
           <div className="content_exp">
             <h3>Technicien systèmes et réseaux</h3>
-            <p>
-              Préfecture de l'AIN - 01000 Bourg-en-Bresse | Mai 2021 - Juillet
-              2021 | Stage
-            </p>
+            <p>Préfecture de l'AIN - 01000 Bourg-en-Bresse | Mai 2021 - Juillet 2021 | Stage</p>
             <li>Déploiement de postes informatiques</li>
             <li>Gestion de tickets</li>
             <li>Gestion d’inventaire.</li>
