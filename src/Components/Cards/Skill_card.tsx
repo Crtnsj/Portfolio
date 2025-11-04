c39146291ea1675bda4e6ddc07b3117a8732925d
crtnsj <crtnsj.pro@gmail.com>
2025-11-04 15:32:06 +0100

    Refactor import skill's logos

--- src/Components/Cards/Skill_card.tsx
+++ src/Components/Cards/Skill_card.tsx
@@ -6,10 +6,12 @@ interface SkillCardProps {
 }
 
 const SkillCard = ({ skillName, rating, logo, color }: SkillCardProps) => {
+  const logo_obj = new URL(`../../assets/${logo}.png`, import.meta.url).href;
+
   const width = 100 - (5 - rating) * 20;
   return (
     <div className="skill">
-      <div className="logo" style={{ backgroundImage: `url(${logo})` }}></div>
+      <div className="logo" style={{ backgroundImage: `url(${logo_obj})` }}></div>
       <div className="progressBar" style={{ "--bar-color": color, "--bar-width": `${width}%` } as React.CSSProperties}></div>
       <p>
         {skillName}: {rating}/5
--- src/Components/Parts/Skills.tsx
+++ src/Components/Parts/Skills.tsx
@@ -1,34 +1,22 @@
 import Timeline from "../Timeline";
 import SkillCard from "../Cards/Skill_card";
 
-import python_logo from "../../assets/logo_python.png";
-import git_logo from "../../assets/logo_git.png";
-import polars_logo from "../../assets/logo_polars.png";
-import selenium_logo from "../../assets/logo_selenium.png";
-import streamlit_logo from "../../assets/logo_streamlit.png";
-import scikit_learn_logo from "../../assets/logo_scikit_learn.png";
-import sql_logo from "../../assets/logo_sql.png";
-import react_logo from "../../assets/logo_react.png";
-import css_logo from "../../assets/logo_css.png";
-import javascript_logo from "../../assets/logo_javascript.png";
-import php_logo from "../../assets/logo_php.png";
-
 const Skills = () => {
   const dataIASkills = [
-    { skillName: "PYTHON", rating: 4, logo: python_logo, color: "#ffd849" },
-    { skillName: "GIT", rating: 3, logo: git_logo, color: "#f05133" },
-    { skillName: "Polars", rating: 3, logo: polars_logo, color: "#2278FA" },
-    { skillName: "Selenium", rating: 4, logo: selenium_logo, color: "#00B500" },
-    { skillName: "Streamlit", rating: 3, logo: streamlit_logo, color: "#FE4C4B" },
-    { skillName: "Scikit Learn", rating: 2, logo: scikit_learn_logo, color: "#F89A39" },
-    { skillName: "SQL", rating: 3, logo: sql_logo, color: "#00546b" },
+    { skillName: "PYTHON", rating: 4, logo: "logo_python", color: "#ffd849" },
+    { skillName: "GIT", rating: 3, logo: "logo_git", color: "#f05133" },
+    { skillName: "Polars", rating: 3, logo: "logo_polars", color: "#2278FA" },
+    { skillName: "Selenium", rating: 4, logo: "logo_selenium", color: "#00B500" },
+    { skillName: "Streamlit", rating: 3, logo: "logo_streamlit", color: "#FE4C4B" },
+    { skillName: "Scikit Learn", rating: 2, logo: "logo_scikit_learn", color: "#F89A39" },
+    { skillName: "SQL", rating: 3, logo: "logo_sql", color: "#00546b" },
   ];
 
   const webSkills = [
-    { skillName: "React", rating: 3, logo: react_logo, color: "#00dcff" },
-    { skillName: "CSS", rating: 4, logo: css_logo, color: "#0277bd" },
-    { skillName: "JavaScript", rating: 2, logo: javascript_logo, color: "#dcb900" },
-    { skillName: "PHP", rating: 3, logo: php_logo, color: "#777bb3" },
+    { skillName: "React", rating: 3, logo: "logo_react", color: "#00dcff" },
+    { skillName: "CSS", rating: 4, logo: "logo_css", color: "#0277bd" },
+    { skillName: "JavaScript", rating: 2, logo: "logo_javascript", color: "#dcb900" },
+    { skillName: "PHP", rating: 3, logo: "logo_php", color: "#777bb3" },
   ];
 
   return (
