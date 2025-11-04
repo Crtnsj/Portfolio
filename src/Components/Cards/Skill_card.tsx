e20e70c95d2fd52fa1a674ee6ce7c7d5273dcc30
crtnsj <crtnsj.pro@gmail.com>
2025-11-04 14:56:39 +0100

    Fix skills logos

--- src/Components/Cards/Skill_card.tsx
+++ src/Components/Cards/Skill_card.tsx
@@ -1,15 +1,15 @@
 interface SkillCardProps {
   skillName: string;
   rating: number;
-  logoName: string;
+  logo: string;
   color: string;
 }
 
-const SkillCard = ({ skillName, rating, logoName, color }: SkillCardProps) => {
+const SkillCard = ({ skillName, rating, logo, color }: SkillCardProps) => {
   const width = 100 - (5 - rating) * 20;
   return (
     <div className="skill">
-      <div className="logo" style={{ backgroundImage: `url("../src/assets/logo_${logoName}.png")` }}></div>
+      <div className="logo" style={{ backgroundImage: `url(${logo})` }}></div>
       <div className="progressBar" style={{ "--bar-color": color, "--bar-width": `${width}%` } as React.CSSProperties}></div>
       <p>
         {skillName}: {rating}/5
